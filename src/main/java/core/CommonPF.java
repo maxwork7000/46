67ad8c9a6981910989be58444727d5e5e1b8d862
Maxwork <maxwork6000@mail.ru>
2019-08-20 09:37:44 -0700

    Second Commit

--- src/main/java/core/CommonPF.java
+++ src/main/java/core/CommonPF.java
@@ -114,56 +114,117 @@ public class CommonPF {
 	// static By el_05 = By.id("id_f_title"); 			// signup & confirmation
 	@FindBy(id="id_f_title") @CacheLookup static WebElement pf_el_05;
 
-	static By el_06 = By.id("id_f_label_fn"); 			// signup & confirmation
-	static By el_07 = By.id("id_fname"); 				// signup & confirmation
+	//static By el_06 = By.id("id_f_label_fn"); 			// signup & confirmation
+	@FindBy(id="id_f_label_fn") @CacheLookup static WebElement pf_el_06;
+	
+	//static By el_07 = By.id("id_fname"); 				// signup & confirmation
+	@FindBy(id="id_fname") @CacheLookup static WebElement pf_el_07;
 
-	static By el_08 = By.id("id_f_label_ln"); 			// signup & confirmation
-	static By el_09 = By.id("id_lname"); 				// signup & confirmation
+	//static By el_08 = By.id("id_f_label_ln"); 			// signup & confirmation
+	@FindBy(id="id_f_label_ln") @CacheLookup static WebElement pf_el_08;
+
+	//static By el_09 = By.id("id_lname"); 				// signup & confirmation
+	@FindBy(id="id_lname") @CacheLookup static WebElement pf_el_09;
 
 	static By el_10 = By.id("id_f_label_ea"); 			// signup & confirmation
-	static By el_11 = By.id("id_email"); 				// signup & confirmation
+	@FindBy(id="id_f_label_ea") @CacheLookup static WebElement pf_el_10;
+
+	//static By el_11 = By.id("id_email"); 				// signup & confirmation
+	@FindBy(id="id_email") @CacheLookup static WebElement pf_el_11;
 
 	static By el_12 = By.id("id_f_label_pn"); 			// signup & confirmation
+	@FindBy(id="id_f_label_pn") @CacheLookup static WebElement pf_el_12;
+
 	static By el_13 = By.id("id_phone"); 				// signup & confirmation
+	@FindBy(id="id_phone") @CacheLookup static WebElement pf_el_13;
 
 	static By el_14 = By.id("id_rb_label_g"); 			// signup & confirmation
+	@FindBy(id="id_rb_label_g") @CacheLookup static WebElement pf_el_14;
 
-	static By el_15 = By.id("id_rb_label_m"); 			// signup
-	static By el_16 = By.id("id_gender_male"); 			// signup
+	//static By el_15 = By.id("id_rb_label_m"); 			// signup
+	@FindBy(id="id_rb_label_m") @CacheLookup static WebElement pf_el_15;
 
-	static By el_17 = By.id("id_rb_label_f"); 			// signup
-	static By el_18 = By.id("id_gender_female"); 		// signup
+	//static By el_16 = By.id("id_gender_male"); 			// signup
+	@FindBy(id="id_gender_male") @CacheLookup static WebElement pf_el_16;
 
-	static By el_19 = By.id("id_f_label_s"); 			// signup & confirmation
-	static By el_20 = By.id("id_state"); 				// signup & confirmation
-	static By el_21 = By.xpath("//*[@id='id_state']/option[6]"); // signup
+	//static By el_17 = By.id("id_rb_label_f"); 			// signup
+	@FindBy(id="id_rb_label_f") @CacheLookup static WebElement pf_el_17;
 
-	static By el_22 = By.id("id_cb_label_t"); 			// signup & confirmation
-	static By el_23 = By.id("id_terms"); 				// signup & confirmation
+	//static By el_18 = By.id("id_gender_female"); 		// signup
+	@FindBy(id="id_gender_female") @CacheLookup static WebElement pf_el_18;
 
-	static By el_24 = By.id("id_img_facebook"); 		// signup
-	static By el_25 = By.id("id_img_twitter"); 			// signup
-	static By el_26 = By.id("id_img_flickr"); 			// signup
-	static By el_27 = By.id("id_img_youtube"); 			// signup
+	//static By el_19 = By.id("id_f_label_s"); 			// signup & confirmation
+	@FindBy(id="id_f_label_s") @CacheLookup static WebElement pf_el_19;
 
-	static By el_28 = By.id("id_reset_button"); 		// signup
-	static By el_29 = By.id("id_submit_button"); 		// signup
+	//static By el_20 = By.id("id_state"); 				// signup & confirmation
+	@FindBy(id="id_state") @CacheLookup static WebElement pf_el_20;
 
-	static By el_30 = By.id("timestamp"); 				// signup
-	static By el_31 = By.id("copyright"); 	// signup & confirmation
-	static By el_32 = By.id("os_browser"); 	// signup
+	//static By el_21 = By.xpath("//*[@id='id_state']/option[6]"); // signup
+	@FindBy(xpath="//*[@id='id_state']/option[6]") @CacheLookup static WebElement pf_el_21;
 
-	static By el_33 = By.id("ErrorLine"); 	// signup
-	static By el_34 = By.id("fname_error"); // signup
-	static By el_35 = By.id("lname_error"); // signup
-	static By el_36 = By.id("email_error"); // signup
-	static By el_37 = By.id("phone_error"); // signup
+	//static By el_22 = By.id("id_cb_label_t"); 			// signup & confirmation
+	@FindBy(id="id_cb_label_t") @CacheLookup static WebElement pf_el_22;
 
-	static By el_38 = By.xpath("//*[@id='fname_error']/img"); // signup
-	static By el_39 = By.xpath("//*[@id='lname_error']/img"); // signup
-	static By el_40 = By.xpath("//*[@id='email_error']/img"); // signup
-	static By el_41 = By.xpath("//*[@id='phone_error']/img"); // signup
+	//static By el_23 = By.id("id_terms"); 				// signup & confirmation
+	@FindBy(id="id_terms") @CacheLookup static WebElement pf_el_23;
 
-	static By el_42 = By.id("id_gender"); 		// confirmation
-	static By el_43 = By.id("id_back_button"); 	// confirmation
+	//static By el_24 = By.id("id_img_facebook"); 		// signup
+	@FindBy(id="id_img_facebook") @CacheLookup static WebElement pf_el_24;
+
+	//static By el_25 = By.id("id_img_twitter"); 			// signup
+	@FindBy(id="id_img_twitter") @CacheLookup static WebElement pf_el_25;
+
+	//static By el_26 = By.id("id_img_flickr"); 			// signup
+	@FindBy(id="id_img_flickr") @CacheLookup static WebElement pf_el_26;
+
+	//static By el_27 = By.id("id_img_youtube"); 			// signup
+	@FindBy(id="id_img_youtube") @CacheLookup static WebElement pf_el_27;
+
+	//static By el_28 = By.id("id_reset_button"); 		// signup
+	@FindBy(id="id_reset_button") @CacheLookup static WebElement pf_el_28;
+
+	//static By el_29 = By.id("id_submit_button"); 		// signup
+	@FindBy(id="id_submit_button") @CacheLookup static WebElement pf_el_29;
+
+	//static By el_30 = By.id("timestamp"); 				// signup
+	@FindBy(id="timestamp") @CacheLookup static WebElement pf_el_30;
+
+	//static By el_31 = By.id("copyright"); 	// signup & confirmation
+	@FindBy(id="copyright") @CacheLookup static WebElement pf_el_31;
+
+	//static By el_32 = By.id("os_browser"); 	// signup
+	@FindBy(id="os_browser") @CacheLookup static WebElement pf_el_32;
+
+	//static By el_33 = By.id("ErrorLine"); 	// signup
+	@FindBy(id="ErrorLine") @CacheLookup static WebElement pf_el_33;
+
+	//static By el_34 = By.id("fname_error"); // signup
+	@FindBy(id="fname_error") @CacheLookup static WebElement pf_el_34;
+
+	//static By el_35 = By.id("lname_error"); // signup
+	@FindBy(id="lname_error") @CacheLookup static WebElement pf_el_35;
+
+	//static By el_36 = By.id("email_error"); // signup
+	@FindBy(id="email_error") @CacheLookup static WebElement pf_el_36;
+
+	//static By el_37 = By.id("phone_error"); // signup
+	@FindBy(id="phone_error") @CacheLookup static WebElement pf_el_37;
+
+	//static By el_38 = By.xpath("//*[@id='fname_error']/img"); // signup
+	@FindBy(xpath="//*[@id='fname_error']/img") @CacheLookup static WebElement pf_el_38;
+
+	//static By el_39 = By.xpath("//*[@id='lname_error']/img"); // signup
+	@FindBy(xpath="//*[@id='lname_error']/img") @CacheLookup static WebElement pf_el_39;
+
+	//static By el_40 = By.xpath("//*[@id='email_error']/img"); // signup
+	@FindBy(xpath="//*[@id='email_error']/img") @CacheLookup static WebElement pf_el_40;
+
+	//static By el_41 = By.xpath("//*[@id='phone_error']/img"); // signup
+	@FindBy(xpath="//*[@id='phone_error']/img") @CacheLookup static WebElement pf_el_41;
+
+	//static By el_42 = By.id("id_gender"); 		// confirmation
+	@FindBy(id="id_gender") @CacheLookup static WebElement pf_el_42;
+
+	//static By el_43 = By.id("id_back_button"); 	// confirmation
+	@FindBy(id="id_back_button") @CacheLookup static WebElement pf_el_43;
 }
